c17059a7eed9aa71427a4782938a961934e3b506
tachyla <tachylad.murray@gmail.com>
2017-05-08 11:51:02 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+const { DATABASE_URL, PORT } = require('./config');
+
+const app = express();
+app.use(bodyParser.json());
+mongoose.connect(DATABASE_URL);
+
+app.get('/', (req, res) => {
+    res.send('hello world!'); 
+});
+
+
+app.listen(PORT, function(){
+    console.log(`Listening on port ${PORT}`);
+});
