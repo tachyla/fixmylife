94427840fd2121d38578b06319b366f9d044e131
tachyla <tachylad.murray@gmail.com>
2017-05-09 08:40:49 -0700

    1 working test

--- server.js
+++ server.js
@@ -61,6 +61,52 @@ app.delete('/add/:id', (req, res) => {
   });
 });
 
-app.listen(PORT, function(){
-  console.log(`Listening on port ${PORT}`);
-});
+
+
+// app.listen(PORT, function(){
+//   console.log(`Listening on port ${PORT}`);
+// });
+
+let server;
+
+function runServer(databaseUrl=DATABASE_URL, port=PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.createConnection(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer(function(err) {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
+
+module.exports = {runServer, app, closeServer};
