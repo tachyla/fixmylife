f376dfaac99813e6fae00ae46b0b97678e5eb044
tachyla <tachylad.murray@gmail.com>
2017-05-09 08:57:21 -0700

    added gitignore

---
+++ config.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/fixmylife';
+exports.PORT = process.env.PORT || 8080;
+
+//exports.apiRoute = process.env.apiRoute;  
