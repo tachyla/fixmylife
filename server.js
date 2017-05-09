37e4a20773f404d412e5e1ec8b6ac06fa9163221
tachyla <tachylad.murray@gmail.com>
2017-05-08 20:09:57 -0700

    imported redditData

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const apiRoute = require('./config');
 const { DATABASE_URL, PORT } = require('./config');
+const {AdviceEntry} = require('./models');
 
 const app = express();
 app.use(bodyParser.json());
 mongoose.connect(DATABASE_URL);
 
-app.get(`/${apiRoute}`, (req, res) => {
-  res.send('hello world!'); 
+app.get('/', (req, res) => {
+  res.json(req.body); 
 });
 
 
@@ -17,6 +18,3 @@ app.listen(PORT, function(){
     console.log(`Listening on port ${PORT}`);
 });
 
-// $.getJSON('', function(res) {
-//     res.json();    
-// });
