84b04d00fd39a6b6e8c589d1d8f707e7a7364ae6
tachyla <tachylad.murray@gmail.com>
2017-05-08 19:32:46 -0700

    latest code

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
-// const { DATABASE_URL, PORT } = require('./config');
+const apiRoute = require('./config');
+const { DATABASE_URL, PORT } = require('./config');
 
 const app = express();
 app.use(bodyParser.json());
-mongoose.connect(process.env.DATABASE_URL);
+mongoose.connect(DATABASE_URL);
 
-app.get('/', (req, res) => {
-    //writes hello world
+app.get(`/${apiRoute}`, (req, res) => {
   res.send('hello world!'); 
 });
 
 
-app.listen(process.env.PORT, function(){
-    console.log(`Listening on port ${process.env.PORT}`);
+app.listen(PORT, function(){
+    console.log(`Listening on port ${PORT}`);
 });
+
+// $.getJSON('', function(res) {
+//     res.json();    
+// });
